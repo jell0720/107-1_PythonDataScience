1cee7056e8b33d413a371b33320afd0b5158e10a
jiangsir <jiangzero@gmail.com>
2019-03-07 13:23:07 +0800

    20190307

---
+++ local/soup.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+result = requests.get('https://www.ptt.cc/bbs/Food/index.html')
+soup = BeautifulSoup(result.text, 'lxml')
+
+selector = "div.title"
+
+tags = soup.select(selector) # 取得一組 bs4.element.Tag 的 list
+
+for tag in tags:
+    print(type(tag))
+    help(tag)
+
+
+
+
