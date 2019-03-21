973b992db1b9c9463d1657e52a23e235fa23383e
jiangsir <jiangzero@gmail.com>
2019-03-21 16:17:01 +0800

    20190321

--- local/soup.py
+++ local/soup.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 result = requests.get('https://www.ptt.cc/bbs/Food/index.html')
 soup = BeautifulSoup(result.text, 'lxml')
 
+soup
+
 selector = "div.title"
 
 tags = soup.select(selector) # 取得一組 bs4.element.Tag 的 list
@@ -10,7 +12,3 @@ tags = soup.select(selector) # 取得一組 bs4.element.Tag 的 list
 for tag in tags:
     print(type(tag))
     help(tag)
-
-
-
-
